c87382c535a7a9497d8a947b89f67f6cc0c4acaa
qinboli <sysulqb@gmail.com>
2019-10-07 11:16:47 -0500

    fix

--- mindfirl/mindfirl.py
+++ mindfirl/mindfirl.py
@@ -805,7 +805,7 @@ def record_linkage_next(pid):
         flask.flash('You have completed the project.', 'alert-success')
         return redirect('project')
 
-    return redirect('record_linkage/'+pid)
+    return redirect(url_for('record_linkage', pid=pid))
 
 
 @app.route('/get_cell', methods=['GET', 'POST'])
